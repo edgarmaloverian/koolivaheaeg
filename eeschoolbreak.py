217e7f654fda55dcfce4d7785b2984988e8270ed
Edgar Maloverian <edgar@maloverian.com>
2023-10-08 14:17:24 +0300

    added 2023/4 year

--- eeschoolbreak.py
+++ eeschoolbreak.py
@@ -1,11 +1,18 @@
 # eeschoolbreak small library signalling if some date is chhool break in Estonia
+# source https://www.riigiteataja.ee/akt/110092020003?leiaKehtiv
+
 from datetime import date
 
 vaheajad = [
-    ["Talvevaheaeg 2022", 22, 12, 2022, 8,  1, 2023],
-    ["Suusavaheaeg 2023", 27, 2,  2023, 5,  3, 2023],
-    ["Kevadvaheaeg 2023", 24, 4,  2023, 30, 4, 2023],
-    ["Suvevaheaeg 2023",  14, 6,  2023, 31, 8, 2023]
+    ["II vaheaeg 2022/3", 22, 12, 2022, 8,  1, 2023],
+    ["III vaheaeg 2022/3", 27, 2,  2023, 5,  3, 2023],
+    ["IV vaheaeg 2022/3", 24, 4,  2023, 30, 4, 2023],
+    ["V vaheaeg 2022/3",  14, 6,  2023, 31, 8, 2023],
+    ["I vaheaeg 2023/4", 23, 10, 2023, 29, 10, 2023],
+    ["II vaheaeg 2023/4", 21, 12, 2023, 7, 1, 2024],
+    ["III vaheaeg 2023/4", 26, 2, 2024, 3, 3, 2024],
+    ["IV vaheaeg 2023/4", 22, 4, 2024, 28, 4, 2024],
+    ["V vaheaeg 2023/4", 13, 6, 2024, 31, 8, 2024],
 ]
             
 def isbreak(a=date.today()): # returns True if it is break today, if you want it to pass another date you need to write argparser
